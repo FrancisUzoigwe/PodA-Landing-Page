f5a214fc563b513ce5a6d0d4b9de2bcc486e501b
podapodcast <153449003+podapodcast@users.noreply.github.com>
2024-01-10 10:09:03 +0100

    Update server host

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
-import cors from "cors";
+import cors from 'cors';
 import fs from 'fs/promises';
 import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+const allowedOrigin = 'https://pod-a-landing-page-dc5q.vercel.app';
+
+const corsOptions = {
+  origin: allowedOrigin,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
@@ -19,23 +25,21 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
-
 app.post('/send-email', async (req, res) => {
-    const { name, email } = req.body;
-    
-    try {
-        // Read the HTML template using async/await
-        const __dirname = path.resolve();
-        const templatePath = path.join(__dirname, './src/email.html');
-        const emailBody = await fs.readFile(templatePath, 'utf8');
-    
-        // Replace placeholders in the template with actual data
-        const formattedEmailBody = emailBody.replace(/{name}/g, name);
+  const { name, email } = req.body;
+
+  try {
+    // Read the HTML template using async/await
+    const __dirname = path.resolve();
+    const templatePath = path.join(__dirname, './src/email.html');
+    const emailBody = await fs.readFile(templatePath, 'utf8');
+
+    // Replace placeholders in the template with actual data
+    const formattedEmailBody = emailBody.replace(/{name}/g, name);
 
     const mailOptions = {
-      from: "podapodcastnew@gmail.com", 
-      to: ["wasiusikiru7@gmail.com",email],
+      from: 'podapodcastnew@gmail.com',
+      to: ['wasiusikiru7@gmail.com', email],
       subject: 'Waitlist Confirmation',
       html: formattedEmailBody,
     };
